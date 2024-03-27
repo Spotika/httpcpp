e187ff019a4d1347ea3e37607758c57124df05a3
Spotika <andrey.a.cherkashin@gmail.com>
2024-03-27 10:05:49 +0300

    feat: add post router to set CORS

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
-#include "../cpp-httplib/httplib.h"
 #include <iostream>
-
-
+#include <json/json.h>
+#include <json/value.h>
+#include "httplib.h"
 
 int main() {
-    
+
     httplib::Server svr;
 
-    svr.Get("/ping", [](const httplib::Request &, httplib::Response &res) {
-    res.set_content("OK", "text/plain");
+    svr.set_post_routing_handler([](const auto& req, auto& res) {
+        res.set_header("Access-Control-Allow-Origin", "*");
+        res.set_header("Access-Control-Allow-Headers", "*");
     });
 
-    svr.listen("0.0.0.0", 8080);
+    svr.Get("/ping", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("OK", "application/json");
+    });
+
+    svr.listen("127.0.0.1", 4242);
 
     return 0;
 }
